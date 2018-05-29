49e8c590465d5ade0ee995c2adb1dcc1179e88d4
Rich Hildred <rhildred@ip-172-31-5-173.us-west-2.compute.internal>
2018-05-29 11:34:35 -0400

    made image be random and have author associated with it

--- App.tsx
+++ App.tsx
@@ -2,12 +2,20 @@ import * as React from 'react';
 import { StyleSheet, Text, View, Image, Button } from 'react-native';
 
 export default class App extends React.Component<{}> {
+  private _choices = [
+    {uri:"https://farm4.staticflickr.com/3088/2688916488_1a125cd0e7_z_d.jpg",
+  author:"Ângela Antunes"},
+    {uri:"https://farm1.staticflickr.com/231/522075744_4aa674bbdd_z_d.jpg",
+    author:"Catherine"}
+  ];
   state = {
-    myImageUri: 'https://facebook.github.io/react-native/docs/assets/favicon.png'
+    myImageUri: this._choices[0].uri,
+    author:this._choices[0].author
   }
   updateImage = () => {
-//    let n = Math.floor(Math.random() * this._choices.length);
-    this.setState({ myImageUri:  "https://farm4.staticflickr.com/3088/2688916488_1a125cd0e7_z_d.jpg"}) //this._choices[n] });
+    let n = Math.floor(Math.random() * this._choices.length);
+    this.setState({ myImageUri: this._choices[n].uri });
+    this.setState({ author: this._choices[n].author });
   }
   render() {
     return (
@@ -19,6 +27,7 @@ export default class App extends React.Component<{}> {
           style={{width: 640, height: 480}}
           source={{uri: this.state.myImageUri}}
         />
+        <Text>Image by: {this.state.author}</Text>
           <Button
             onPress={this.updateImage}
             title="Learn More"
