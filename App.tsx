f025c7ee99b6aa1b174361738370b1b600b90f80
Rich Hildred <rhildred@ip-172-31-5-173.us-west-2.compute.internal>
2018-05-29 10:51:06 -0400

    made the image change when I added and clicked on a button

--- App.tsx
+++ App.tsx
@@ -1,13 +1,30 @@
 import * as React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, Button } from 'react-native';
 
 export default class App extends React.Component<{}> {
+  state = {
+    myImageUri: 'https://facebook.github.io/react-native/docs/assets/favicon.png'
+  }
+  updateImage = () => {
+//    let n = Math.floor(Math.random() * this._choices.length);
+    this.setState({ myImageUri:  "https://farm4.staticflickr.com/3088/2688916488_1a125cd0e7_z_d.jpg"}) //this._choices[n] });
+  }
   render() {
     return (
       <View style={styles.container}>
         <Text>Open up App.ts to start working on your app!</Text>
         <Text>Changes you make will automatically reload.</Text>
         <Text>Shake your phone to open the developer menu.</Text>
+        <Image
+          style={{width: 640, height: 480}}
+          source={{uri: this.state.myImageUri}}
+        />
+          <Button
+            onPress={this.updateImage}
+            title="Learn More"
+            accessibilityLabel="Learn more about this purple button"
+          />
+
       </View>
     );
   }
